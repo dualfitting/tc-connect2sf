98b3d0ac4c156049b5ad80432b7944a930bb592c
Mauricio Desiderio <mdesiderio@appirio.com>
2016-10-28 11:46:44 -0500

    cloudformation

--- consumer/cloudformation/update-stack.sh
+++ consumer/cloudformation/update-stack.sh
@@ -1,1 +1,1 @@
-aws --profile $1 cloudformation update-stack --stack-name tc-connect2sf --template-body file://`pwd`/tc-connect2sf-ecs-cfn.json --capabilities CAPABILITY_IAM
+aws --profile $1 cloudformation update-stack --stack-name tc-connect2sf --template-body file://`pwd`/tc-connect2sf-ecs-cfn-dev.json --capabilities CAPABILITY_IAM
