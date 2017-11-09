dab2cd18a792909d5ff83f307be0a0d774c0c107
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-11-09 16:19:33 +0530

    debug logging

--- consumer/src/services/ConsumerService.js
+++ consumer/src/services/ConsumerService.js
@@ -83,6 +83,7 @@ class ConsumerService {
       const campaignId = responses[0];
       const user = responses[1];
       const { accessToken, instanceUrl } = responses[2];
+      console.log(project, 'project')
       const lead = {
         FirstName: user.firstName,
         LastName: user.lastName,
